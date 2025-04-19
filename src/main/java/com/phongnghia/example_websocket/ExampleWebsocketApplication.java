693dde3300b5a2b574ef6722fa2c2620f2f95b67
nghia pham <phamhongnghia13579@gmail.com>
2025-04-19 21:48:03 +0700

    init project

---
+++ src/main/java/com/phongnghia/example_websocket/ExampleWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.phongnghia.example_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phongnghia/example_websocket/ExampleWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phongnghia.example_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
